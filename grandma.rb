65183cded31f44e5d930e42c1ea5c1932f8b19ec
Justin Phillips <justinphil02@yahoo.com>
2019-10-09 00:55:43 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,43 @@
-# Write a speak_to_grandma method.
+#def all_upper(string)
+#a = string
+#b = string.upcase
+#  if a == b 
+#  return TRUE
+#  else
+#  return FALSE
+#  end
+#end
+
+def speak_to_grandma(string)
+a = []
+a=string.chars
+gma_can_hear = TRUE
+i = 0 
+
+if string.upcase == string 
+  gma_can_hear = true 
+else
+  gma_can_hear = false 
+end 
+  
+  if gma_can_hear == false
+    responce = "HUH?! SPEAK UP, SONNY!"
+  elsif string == "I LOVE YOU GRANDMA!"
+     responce = "I LOVE YOU TOO PUMPKIN!"
+  else
+     responce = "NO, NOT SINCE 1938!"
+  end 
+
+puts responce
+return responce
+
+
+end
+
+
+
+  
+  # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
